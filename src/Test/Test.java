bc20c58813b97756f53c4ca4213539e456441596
Norkerz <reidelor@gmail.com>
2026-05-04 15:53:46 +0200

    mmmmmmmmmmmmmmm

--- src/Test/Test.java
+++ src/Test/Test.java
@@ -2,11 +2,6 @@ package Test;
 
 public class Test {
 
-<<<<<<< HEAD
-	//I love kittens
-	//Meow ig
-=======
-	//I love miners
->>>>>>> branch 'main' of https://github.com/Trewsaazz/BoBert_Royale.git
+
 	
 }

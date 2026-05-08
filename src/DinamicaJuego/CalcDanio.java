ce60f3403fb88c11b23947c2a36bc48325a2250b
God-Basilisk <luisrubiojimenez5@gmail.com>
2026-05-08 20:34:34 +0200

    Singleton que calcula el danio Version 1

---
+++ src/DinamicaJuego/CalcDanio.java
@@ -0,0 +1,26 @@
+package DinamicaJuego;
+
+public class CalcDanio {
+	
+	private static CalcDanio instance;
+	
+	private CalcDanio() {
+	}
+	
+	public static CalcDanio getInstance(){
+		if(instance == null) {
+			instance = new CalcDanio();
+		}
+		return instance;
+	}
+	
+	public int calc(int atc, int def) {
+		int ans;
+		ans = atc - def;
+		if(ans<0) {
+			return 0;
+		}else {
+			return ans;
+		}
+	}
+}

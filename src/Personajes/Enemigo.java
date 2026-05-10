6ee4c742a0a7baa58593092e51c725c0d1ca8b11
Norkerz <reidelor@gmail.com>
2026-05-10 04:04:17 +0200

    Inicio de estrategias en enemigo

--- src/Personajes/Enemigo.java
+++ src/Personajes/Enemigo.java
@@ -1,11 +1,22 @@
 package Personajes;
 
+import Estrategia.IEstrategia;
+import Armas.IArmas;
+
 public abstract class Enemigo {
 
 	private int fuerza;
 	private int resistencia;
 	private int agilidad;
 	private int vida = 100;
+	
+	
+	private IEstrategia estrategia;
+	private IArmas arma;
+	
+	/*TODO implementar las estrategias (nico)*/ 
+	
+	
 	
 	public int getFuerza() {
 		
